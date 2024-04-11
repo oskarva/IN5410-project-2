a1f895fe5157a1baf7662d6d840b4e9ed8a191fe
Oskar Våle <oskarva@pop-os.localdomain>
2024-04-11 14:14:12 +0200

    added data loading helper functions and linreg code

--- helper_functions.py
+++ helper_functions.py
@@ -1,1 +1,17 @@
+import pandas as pd
 #Any functions common for two or more files may be stored here
+
+def get_train_data() -> pd.DataFrame: 
+    train_data = pd.read_csv("TrainData.csv")
+    train_data.set_index('TIMESTAMP', inplace=True)
+    return train_data
+
+def get_test_data_X() -> pd.DataFrame:
+    test_data_X = pd.read_csv("WeatherForecastInput.csv")
+    test_data_X.set_index('TIMESTAMP', inplace=True)
+    return test_data_X
+
+def get_test_data_Y() -> pd.DataFrame:
+    test_data_Y = pd.read_csv("Solution.csv")
+    test_data_Y.set_index('TIMESTAMP', inplace=True)
+    return test_data_Y
--- part_1.py
+++ part_1.py
@@ -1,3 +1,4 @@
+from helper_functions import *
 from sklearn.linear_model import LinearRegression
 
 #Training data = TrainData.csv, ONLY 10m above ground level
@@ -5,10 +6,27 @@ from sklearn.linear_model import LinearRegression
 #Test data = WeatherForecastInput.csv (test inputs) and Solution.csv (gold standard)
 #Generates: four output .csv files 
 def part_1():
+
+    #load the training data
+    train_data = get_train_data()
+
+    #Only need windspeed data (10 meters above ground level)
+    train_X = train_data[['WS10']]
+    train_Y = train_data[['POWER']]
+
+    #load the test data
+    test_X = get_test_data_X()
+    test_X = test_X[['WS10']]
+    test_Y = get_test_data_Y()
+
     #1. Linear regression
     #NOTE: fit_intercept=True by default. If the data is centered, set to False
     lin_reg_model = LinearRegression()
-    #lin_reg_model.fit(, y)
+    lin_reg_model.fit(train_X, train_Y)
+
+    predictions = lin_reg_model.predict(test_X)
+    score = lin_reg_model.score(predictions, test_Y)
+    print(score)
 
     #2. KNN regression
 
@@ -26,3 +44,5 @@ def part_1():
     """
     return None
 
+if __name__ == "__main__":
+    part_1()
