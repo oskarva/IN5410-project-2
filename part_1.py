238f8e016278839f78924ba1b8bf41c3b9c36a96
Oskar Våle <oskarva@pop-os.localdomain>
2024-04-12 20:49:57 +0200

    added gitignore, changes axis titles, removed csvs

--- part_1.py
+++ part_1.py
@@ -95,7 +95,7 @@ def part_1():
     # KNN Regression
     plt.subplot(2, 2, 2)
     plt.plot(test_X.index, test_Y, label='True')
-    plt.plot(test_X.index, knn_model.predict(test_X), label='KNN Regression')
+    plt.plot(test_X.index, knn_model.predict(test_X), label='KNN')
     plt.xlabel('Date')
     plt.ylabel('Wind Energy')
     plt.title('KNN Regression')
@@ -104,7 +104,7 @@ def part_1():
     # SVR Regression
     plt.subplot(2, 2, 3)
     plt.plot(test_X.index, test_Y, label='True')
-    plt.plot(test_X.index, svr_model.predict(test_X), label='SVR Regression')
+    plt.plot(test_X.index, svr_model.predict(test_X), label='SVR')
     plt.xlabel('Date')
     plt.ylabel('Wind Energy')
     plt.title('SVR Regression')
@@ -113,7 +113,7 @@ def part_1():
     # Neural Network Regression
     plt.subplot(2, 2, 4)
     plt.plot(test_X.index, test_Y, label='True')
-    plt.plot(test_X.index, neural_network.predict(test_X), label='Neural Network Regression')
+    plt.plot(test_X.index, neural_network.predict(test_X), label='Neural Network')
     plt.xlabel('Date')
     plt.ylabel('Wind Energy')
     plt.title('Neural Network Regression')
