5182b4c03eeda5ce8d41a549a7eac21652a9f7fd
Oskar Våle <oskarva@pop-os.localdomain>
2024-04-16 12:44:36 +0200

    Loaded data in part 3

--- part_3.py
+++ part_3.py
@@ -1,6 +1,20 @@
+from helper_functions import *
 
 def part_3():
-    return None
+    #load the training data
+    #train_data = get_train_data()
+
+    #Only need windspeed data (10 meters above ground level)
+    train_data = pd.read_csv("TrainData.csv")
+    train_X = train_data[['TIMESTAMP']]
+    train_Y = train_data[['POWER']]
+
+    #load the test data
+    test_X = pd.read_csv("WeatherForecastInput.csv")
+    test_X = test_X[['TIMESTAMP']]
+    test_Y = get_test_data_Y()
+
+    #1. Linear regression
 
 if __name__ == "__main__":
     part_3()
