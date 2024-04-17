3848e4fa0b72be671d21feb0890a1b15aef4ee5b
Oskar Våle <oskarva@pop-os.localdomain>
2024-04-17 14:46:43 +0200

    weight initialization

--- part_4.py
+++ part_4.py
@@ -8,14 +8,19 @@ The class will be able to have an adjustable number of layers and nodes in all l
 def part_4():
     train_X = [[0.04, 0.2]] #x1 and x2
     train_Y = [0.5] #y1
-    layers = [2, 2, 1]
+    layers = [NN_layer(nodes=2, input_layer=True), 
+              NN_layer(nodes=2),
+              NN_layer(nodes=1, output_layer=True),
+              ]
+    nn = NN(layers)
 
 class NN:
     def __init__(self, layers) -> None:
         self._layers = layers
         if layers[-1] != 1: #Enforce 1 output node for simplicity
             self._layers[-1] = 1
-    
+        self._learning_rate = 0.4
+
     def fit(self, X, Y):
         return None
     
@@ -33,13 +38,16 @@ class NN:
         return predictions
 
 class NN_layer:
-    def __init__(self):
-        self._learning_rate = 0.4
+    def __init__(self, nodes, input_size, output_size, input_layer=False, output_layer=False) -> None:
+        self._weights = None if input_layer or output_layer \
+                        else np.random.rand(nodes, input_size)
+        self._input_layer = input_layer
+        self._output_layer = output_layer
     
     def _forward_propagation(self, X):
         return None
 
-    def _back_propagation(self, X, Y):
+    def _back_propagation(self, X, Y, learning_rate):
         return None
     
     def activation(self, x):
