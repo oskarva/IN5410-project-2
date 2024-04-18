d75ebaca8ecf921424ada799f396e1508eeefa0c
Oskar Våle <oskarva@pop-os.localdomain>
2024-04-18 19:23:26 +0200

    Making things prettier :sparkles:

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from part_3 import part_3
 from part_4 import part_4
 
 def main():
+    print("Running all parts")
+
+    print("\n-----------Part 1-----------")
     part_1()
+    
+    print("\n-----------Part 2-----------")
     part_2()
+    
+    print("\n-----------Part 3-----------")
     part_3()
+
+    print("\n-----------Part 4-----------")
     part_4()
 
 if __name__ == "__main__":
